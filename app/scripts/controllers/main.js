b50eb95bdc02ba2571f084b848a762e57ec55279
Vibhor Mahajan <vibhor.mahajan@gmail.com>
2014-04-09 10:55:39 +0530

    Layout updates to how the episodes look on the home page

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,12 +1,17 @@
 'use strict';
 
 angular.module('ngcastsApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.episodes = [
-      {
-        number: 1,
-        title: "Creating an Angular app using Yeoman",
-        description: "Yeoman is awesome. yeoman.io"
-      }
-    ];
-  });
+    .controller('MainCtrl', function ($scope) {
+      $scope.episodes = [
+        {
+          number: 1,
+          title: "Creating an Angular app using Yeoman",
+          description: "Yeoman helps you get started with AngularJS quickly by scaffolding workflows, while at the same time mixing in many of the best practices that have evolved within the industry."
+        },
+        {
+          number: 1,
+          title: "Creating an Angular app using Yeoman",
+          description: "Yeoman helps you get started with AngularJS quickly by scaffolding workflows, while at the same time mixing in many of the best practices that have evolved within the industry."
+        }
+      ];
+    });
