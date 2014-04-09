114b0011214f56ee98d69849c5a7897a1df61d80
Vibhor Mahajan <vibhor.mahajan@gmail.com>
2014-04-09 10:19:14 +0530

    First look

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,18 +1,19 @@
 'use strict';
 
 angular
-  .module('ngcastsApp', [
-    'ngResource',
-    'ngSanitize',
-    'ngRoute'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .module('ngcastsApp', [
+      'ngResource',
+      'ngSanitize',
+      'ngRoute',
+      'ui.bootstrap'
+    ])
+    .config(function ($routeProvider) {
+      $routeProvider
+          .when('/', {
+            templateUrl: 'views/main.html',
+            controller: 'MainCtrl'
+          })
+          .otherwise({
+            redirectTo: '/'
+          });
+    });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,11 @@
 
 angular.module('ngcastsApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.episodes = [
+      {
+        number: 1,
+        title: "Creating an Angular app using Yeoman",
+        description: "Yeoman is awesome. yeoman.io"
+      }
     ];
   });
