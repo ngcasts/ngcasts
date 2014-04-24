474164803013a4264139e022b7f1eca082f84736
Vibhor Mahajan <vibhor.mahajan@gmail.com>
2014-04-24 23:01:59 +0530

    Created a directive to control link active toggle on path change

--- app/scripts/controllers/application.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('ngcastsApp')
-    .controller('ApplicationCtrl', function ($scope, $location) {
-      $scope.isActive = function (route) {
-        return route === $location.path();
-      };
-    });
---
+++ app/scripts/directives/ngcactiveifpath.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('ngcastsApp')
+    .directive('ngcActiveIfPath', function ($location) {
+      return {
+        restrict: 'A',
+        link: function postLink(scope, element, attrs) {
+          var checkActivation = function () {
+            if (attrs.ngcActiveIfPath === $location.path()) {
+              element.addClass("active");
+            } else {
+              element.removeClass("active");
+            }
+          }
+          scope.$on('$routeChangeSuccess', checkActivation);
+        }
+      };
+    });
